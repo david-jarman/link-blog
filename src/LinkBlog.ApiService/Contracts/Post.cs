bb4b47e72ffa41b810453048b88236d2edc7eff2
David Jarman <davidbjarman@gmail.com>
2025-02-11 11:59:58 -0800

    Add Post model to LinkBlog.ApiService

---
+++ src/LinkBlog.ApiService/Contracts/Post.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkBlog.ApiService;
+
+public sealed class Post
+{
+    public Post(
+        string id,
+        string title,
+        DateTimeOffset date,
+        string? url = null,
+        string? contents = null,
+        IReadOnlySet<string>? tags = null)
+    {
+        Id = id;
+        Title = title;
+        Date = date;
+        Url = url;
+        Contents = contents;
+        Tags = tags ?? new HashSet<string>();
+    }
+
+    public string Id { get; }
+    public string Title { get; }
+    public DateTimeOffset Date { get; }
+    public string? Url { get; }
+    public string? Contents { get; }
+    public IReadOnlySet<string> Tags { get; }
+}
