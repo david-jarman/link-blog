4a425ef4be3a5c7a22c6ec07f1a1a13e858807a7
David Jarman <davidbjarman@gmail.com>
2025-02-15 19:33:42 -0800

    Create claims from calling github user api

--- src/LinkBlog.Web/Security/GitHubAccountHandler.cs
+++ src/LinkBlog.Web/Security/GitHubAccountHandler.cs
@@ -1,4 +1,7 @@
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text.Encodings.Web;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Options;
@@ -10,6 +13,26 @@ public sealed class GitHubAccountHandler : OAuthHandler<GitHubAccountOptions>
     public GitHubAccountHandler(IOptionsMonitor<GitHubAccountOptions> options, ILoggerFactory logger, UrlEncoder encoder)
         : base(options, logger, encoder)
     {
+    }
+
+    protected override async Task<AuthenticationTicket> CreateTicketAsync(ClaimsIdentity identity, AuthenticationProperties properties, OAuthTokenResponse tokens)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+
+        var response = await Backchannel.SendAsync(request, Context.RequestAborted);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"An error occurred when retrieving GitHub user information ({response.StatusCode}).");
+        }
+
+        using (var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync(Context.RequestAborted)))
+        {
+            var context = new OAuthCreatingTicketContext(new ClaimsPrincipal(identity), properties, Context, Scheme, Options, Backchannel, tokens, payload.RootElement);
+            context.RunClaimActions();
+            await Events.CreatingTicket(context);
+            return new AuthenticationTicket(context.Principal!, context.Properties, Scheme.Name);
+        }
     }
 }
 
@@ -24,6 +47,10 @@ public sealed class GitHubAccountOptions : OAuthOptions
         UsePkce = false; // GitHub doesn't support PKCE
 
         Scope.Add("read:user");
+
+        ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+        ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+        ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
     }
 }
 
