e0537cefd098b83ffafda4b78b09851527e381f1
David Jarman <davidbjarman@gmail.com>
2025-02-23 14:00:03 -0800

    Enable sensitive data logging in development for PostDbContext

--- src/LinkBlog.Data/Extensions/IHostApplicationBuilderExtensions.cs
+++ src/LinkBlog.Data/Extensions/IHostApplicationBuilderExtensions.cs
@@ -8,7 +8,13 @@ public static class ServiceCollectionExtensions
 {
     public static IHostApplicationBuilder AddPostStore(this IHostApplicationBuilder app, string connectionName, Action<NpgsqlEntityFrameworkCorePostgreSQLSettings>? configureSettings)
     {
-        app.AddNpgsqlDbContext<PostDbContext>(connectionName, configureSettings);
+        app.AddNpgsqlDbContext<PostDbContext>(connectionName, configureSettings, dbContextBuilder =>
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                dbContextBuilder.EnableSensitiveDataLogging();
+            }
+        });
         app.Services.AddScoped<IPostStore, PostStoreDb>();
 
         return app;
