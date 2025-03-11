d1722f9a98282a8159b0b181c1ec399d3b9bace1
David Jarman <davidbjarman@gmail.com>
2025-03-10 22:33:02 -0700

    Refactor Trix integration: change variable name and remove event listener

--- src/LinkBlog.Web/index.js
+++ src/LinkBlog.Web/index.js
@@ -1,8 +1,1 @@
-const trix = require('trix');
-
-document.addEventListener("trix-before-initialize", () => {
-    // Change Trix.config if you need
-    console.log("trix-before-initialize");
-    console.log(Trix);
-    console.log(Trix.config);
-})
+const _ = require('trix');
