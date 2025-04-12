60a7ca36eaf0d1ac3e0aeea44355a00aa936b7f1
David Jarman <davidbjarman@gmail.com>
2025-04-11 20:14:16 -0700

    Move common build properties to central location. Enable style analysis in command line build

--- src/LinkBlog.ServiceDefaults/Extensions.cs
+++ src/LinkBlog.ServiceDefaults/Extensions.cs
@@ -9,9 +9,9 @@ using OpenTelemetry.Trace;
 
 namespace Microsoft.Extensions.Hosting;
 
-// Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
-// This project should be referenced by each service project in your solution.
-// To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
+/// <summary>
+/// Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
+/// </summary>
 public static class Extensions
 {
     public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
