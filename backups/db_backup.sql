ac276abcb28b2f1c1445714cc9bb3ed53e1e01e4
GitHub Action <action@github.com>
2025-09-05 08:07:41 +0000

    Database backup: 2025-09-05 08:07:41

--- backups/db_backup.sql
+++ backups/db_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict KTXqF7nLKCi0g8XDoHS4cFEd4OWTldd47Ema0wYcGyT0TYkKYhwhGwoplp3b0VT
+\restrict C0I5YoHN33MlHCcGZl1hfxfSZBtSE8D9g5S6XVHffq07WSliO3aNVGK1eTuCpuW
 
 -- Dumped from database version 16.8
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
@@ -228,5 +228,5 @@ COPY public."__EFMigrationsHistory" ("MigrationId", "ProductVersion") FROM stdin
 -- PostgreSQL database dump complete
 --
 
-\unrestrict KTXqF7nLKCi0g8XDoHS4cFEd4OWTldd47Ema0wYcGyT0TYkKYhwhGwoplp3b0VT
+\unrestrict C0I5YoHN33MlHCcGZl1hfxfSZBtSE8D9g5S6XVHffq07WSliO3aNVGK1eTuCpuW
 
