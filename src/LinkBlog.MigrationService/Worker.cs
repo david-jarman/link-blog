69bd33bf7555e654abd3cb857af6ac3296f950b3
David Jarman <davidbjarman@gmail.com>
2026-02-01 19:48:33 -0800

    simplify migration worker

--- src/LinkBlog.MigrationService/Worker.cs
+++ src/LinkBlog.MigrationService/Worker.cs
@@ -1,8 +1,6 @@
 using System.Diagnostics;
 using LinkBlog.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace LinkBlog.MigrationService;
 
@@ -21,8 +19,7 @@ public class Worker(
             using var scope = serviceProvider.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<PostDbContext>();
 
-            await EnsureDatabaseAsync(dbContext, stoppingToken);
-            await RunMigrationAsync(dbContext, stoppingToken);
+            await dbContext.Database.MigrateAsync(stoppingToken);
         }
         catch (Exception ex)
         {
@@ -32,32 +29,5 @@ public class Worker(
 
         // Stop the application once migration is complete
         hostApplicationLifetime.StopApplication();
-    }
-
-    private static async Task EnsureDatabaseAsync(PostDbContext dbContext, CancellationToken stoppingToken)
-    {
-        var dbCreator = dbContext.GetService<IRelationalDatabaseCreator>();
-
-        var strategy = dbContext.Database.CreateExecutionStrategy();
-        await strategy.ExecuteAsync(async () =>
-        {
-            // Create the database if it does not exist
-            if (!await dbCreator.ExistsAsync(stoppingToken))
-            {
-                await dbCreator.CreateAsync(stoppingToken);
-            }
-        });
-    }
-
-    private static async Task RunMigrationAsync(PostDbContext dbContext, CancellationToken stoppingToken)
-    {
-        var strategy = dbContext.Database.CreateExecutionStrategy();
-        await strategy.ExecuteAsync(async () =>
-        {
-            // Run migration in a transaction to avoid partial migration on failure
-            await using var transaction = await dbContext.Database.BeginTransactionAsync(stoppingToken);
-            await dbContext.Database.MigrateAsync(stoppingToken);
-            await transaction.CommitAsync(stoppingToken);
-        });
     }
 }
